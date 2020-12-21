aead9b440ed085e26f8035bfc52ba19ef4f9b79a
Polina <polinaalejnikowa@gmail.com>
2020-12-21 21:17:52 +0300

    change page_2

--- js/page_1.js
+++ js/page_1.js
@@ -1,5 +1,16 @@
-let btnBlockFunctionalTraing = document.getElementById('btnBlockFunctionalTraing');
-
-btnBlockFunctionalTraing.onclick = () => {
-    window.location.href = '../page_2.html'
-}
+let swiper = new Swiper('.swiper-container', {
+    spaceBetween: 0,
+    width: 750,
+    loop: false,
+    freeMode: false,
+    slidesPerView: 3,
+    spaceBetween: 25,
+    centeredSlidesBounds: true,
+    slidesPerGroup : 1,
+    watchSlidesProgress: true,      
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    centeredSlides: false,
+});
